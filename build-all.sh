68d01cd71895c09ee7a20fefd5a104fb5a1b23b7
eugene patsiomkin <eugene@atvenu.com>
2025-05-14 16:00:05 -0700

    feat: cleanup

---
+++ build-all.sh
@@ -0,0 +1,3 @@
+./build.sh --nodev 16 --npmv 6
+./build.sh --nodev 18 --npmv 10
+./build.sh --nodev 20 --npmv 10
--- build.sh
+++ build.sh
@@ -1,21 +1,31 @@
+#!/bin/bash
+nodev=18
+npmv=10
+
+parse_args() {
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            --nodev) nodev="$2"; shift ;;
+            --npmv) npmv="$2"; shift ;;
+            --help|-h)
+                echo "Usage: $0 [--nodev <node version>] [--npmv <npm version>]"
+                exit 0
+                ;;
+            *)
+                echo "Unknown parameter: $1"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+}
+
+parse_args "$@"
+
 docker build -f dockerfiles/Dockerfile.base -t nvm-node-base .
 
 docker buildx build -f dockerfiles/Dockerfile.node \
-  -t nvm-node:18 \
-  -t nvm-node:default \
-  --build-arg NODE_VERSION=18 \
-  --build-arg NPM_VERSION=10 \
-  .
-
-docker buildx build -f dockerfiles/Dockerfile.node \
-  -t nvm-node:16 \
-  -t nvm-node:depricated \
-  --build-arg NODE_VERSION=16 \
-  --build-arg NPM_VERSION=6 \
-  .
-
-docker buildx build -f dockerfiles/Dockerfile.node \
-  -t nvm-node:20 \
-  --build-arg NODE_VERSION=20 \
-  --build-arg NPM_VERSION=10 \
+  -t nvm-node:"$nodev" \
+  --build-arg NODE_VERSION="$nodev" \
+  --build-arg NPM_VERSION="$npmv" \
   .
