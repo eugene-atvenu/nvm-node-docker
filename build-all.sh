e375a7d7dcaea758b20f8882bd3663c13fded838
eugene patsiomkin <eugene@atvenu.com>
2025-06-05 16:23:58 -0700

    fix: permissions

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,4 @@
-./build.sh --node 16.13.0 --npm 6.14
+# ./build.sh --node 16.13.0 --npm 6.14
 ./build.sh --node 18.20.8 --npm 10.9
 # ./build.sh --node 20 --npm 10
 # ./build.sh --node 16 --npm 6 --pm2 true
